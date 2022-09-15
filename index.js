b67fbc8d9523559cbeb33f68ae8524dd3a854232
piyush yadav <84402719+piyushyadav0191@users.noreply.github.com>
2022-09-15 11:27:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app); // Add this
 // Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chattty-nine.vercel.app/',
     methods: ['GET', 'POST'],
   },
 });
